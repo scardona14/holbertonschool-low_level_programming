eca2f1a46abc9ae4ff97b3df31d0e96bfed94487
scardona14 <cardonasean123@gmail.com>
2023-10-16 13:59:31 -0400

    printalpha commit

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,22 +7,15 @@
 */
 int main(void) 
 {
-	int i;
-	int j;
+	char c;
 
-	for (i = 10; i <= 19; i++)
-	{
-		for (j=10; j<= 19; j++)
-		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-			if (i != 19 || j != 19)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+	c = 'a';
+	while
+		(c<= 'z') 
+{
+		putchar(c);
+		c++;
+}
 	putchar('\n');
 	return (0);
 }
