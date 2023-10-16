3f8b3538f4fee3a1c954e89de613052215920ae9
scardona14 <cardonasean123@gmail.com>
2023-10-16 16:24:42 -0400

    printtrevalphabt commit

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
-* main entry point 
+* main - entry point 
 *
-* Return alway returns 0 
+* Return: alway returns 0 
 *
 */
 int main(void)
 {
 	char a;
-	
 	for (a = 'z'; a >= 'a' ; a--)
 		putchar(a);
 	putchar('\n');
