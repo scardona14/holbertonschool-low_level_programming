af509be66722169e7afaccd7c9ece8d0f0537a69
scardona14 <cardonasean123@gmail.com>
2023-10-17 11:39:41 -0400

    allphabet commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+ #include "main.h" 
+/**
+* print_alphabet - print alphabet
+*
+* Return: return alway 0 
+*
+*/
+void print_alphabet(void)
+{
+	char i;
+	
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
