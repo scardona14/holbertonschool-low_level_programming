e7e9e7d232f2579ccce94da588dc05e2222657b1
scardona14 <cardonasean123@gmail.com>
2023-10-16 21:31:27 -0400

    combprrrinnt commit

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,12 @@ int main(void)
 	{
 		for (j = 10; j <= 19; j++)
 		{
-			if ((j % 10) > (i % 10))
-			{		
+			if ((j % 10) > (i % 10)){
+
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
 				if (i != 18 || j != 19)
-				{	
+				{
 					putchar(',');
 					putchar(' ');
 				}
