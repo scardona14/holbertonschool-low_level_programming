f3fbb614dbb5cbe675088edaa6a81324e2e619ac
scardona14 <cardonasean123@gmail.com>
2023-10-17 11:44:35 -0400

    fixing whitespace

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
- #include "main.h" 
+ #include "main.h"
 /**
 * print_alphabet - print alphabet
 *
-* Return: return alway 0 
+* Return: return alway 0
 *
 */
 void print_alphabet(void)
 {
 	char i;
-	
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
