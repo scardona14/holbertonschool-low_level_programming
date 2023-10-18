2c11d2c682b2d72259a0c554e00b22b734329c09
scardona14 <cardonasean123@gmail.com>
2023-10-18 11:10:25 -0400

    printto98 commit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
-
+/**
+* print_to_98 - Prints all natural number from n to 98.
+* @n: The starting number
+*
+* Description: This function prints all natural numbers from n to 98,
+*		including both n and 98.
+*/
 void print_to_98(int n)
 {
 	int i;
