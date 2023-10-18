4ab822841c90f2814ff537b6ef43c5cb59cd7313
scardona14 <cardonasean123@gmail.com>
2023-10-18 10:57:48 -0400

    printto98 commit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,11 +6,12 @@ void print_to_98(int n)
 {
 	int i;
 	if (n > 98)
+
 	{
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
-			if(i != 98)
+			if (i != 98)
 			{
 				printf(", ");
 			}
@@ -21,7 +22,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
-			if (i !=98)
+			if (i != 98)
 			{
 				printf(", ");
 			}
