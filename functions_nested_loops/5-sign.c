412acc9cc42debe5de9c857993d3ea11a52602dd
scardona14 <cardonasean123@gmail.com>
2023-10-17 22:29:28 -0400

    sign5 commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_sign - main function
+*
+* @n: character to compare
+*
+* Return: 1 if postive, 0 if zero, -1 if negative
+*
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+		_putchar('-');
+		return (-1);
+}
