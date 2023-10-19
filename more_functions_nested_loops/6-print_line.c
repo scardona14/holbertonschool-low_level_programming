8a06884a7a01620a3bce2a0f9c9c86bcda53437f
scardona14 <cardonasean123@gmail.com>
2023-10-19 13:53:46 -0400

    printline commit

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-* print_line - prints a line
-*
-* @n: integer to print stuff?
-*/
+ * print_line - prints a line
+ *
+ * @n: integer to print stuff?
+ */
 void print_line(int n)
 {
 	int i;
-	
-	for (i = '0'; i < n; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		_putchar('_');
 	}
