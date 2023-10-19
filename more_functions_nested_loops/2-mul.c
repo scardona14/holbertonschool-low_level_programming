42c76e0a129a2914da214ebfe3b0b974ec58e18a
scardona14 <cardonasean123@gmail.com>
2023-10-19 11:20:02 -0400

    multplication commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* mul - Multiple two integers
+*@a: first operand
+*@b: second operand
+*
+* Return: product of both operands
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
