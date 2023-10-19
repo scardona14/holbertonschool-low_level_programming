d2f66f64b56621338569e6b165f6885db16fb06f
scardona14 <cardonasean123@gmail.com>
2023-10-19 10:53:35 -0400

    isdigitss commit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
-* _isdigit - checks for digits 0 trough 9
-*@c: int to be check
-*
-* Return 1 if c is a digit, 0 otherwise
-*/
+ * _isdigit - checks for digits (0 trough 9)
+ * @c: int to be check
+ * Return 1 if c is a digit, 0 otherwise
+ */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
