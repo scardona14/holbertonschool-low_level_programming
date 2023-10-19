afa60b63be87b9c938c750719ae222f8c21bf9bc
scardona14 <cardonasean123@gmail.com>
2023-10-19 14:32:14 -0400

    primefacot commit

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+* main - Prints the largest prime factor of 612852475143
+*
+* Return: 0
+*/
+int main(void)
+{
+	long int n = 612852475143;
+	int maxPrime, oddNum;
+
+	while (n % 2 == 0)
+	{
+		maxPrime = 2;
+		n /= 2;
+	}
+	for (addNum = 3; oddNum <= sqrt(n); oddNum += 2)
+	{
+		while (n % oddNum == 0)
+		{
+			maxPrime = oddNum;
+			n /= oddNum;
+		}
+	}
+	if (n > 2)
+		maxPrime = n;
+	printf("i\n", maxPrime);
+	return (0);
+}
