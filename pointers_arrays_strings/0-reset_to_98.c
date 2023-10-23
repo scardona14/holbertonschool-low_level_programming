75be4636f109fc59e7fc23e8e81409247a0fc82c
scardona14 <cardonasean123@gmail.com>
2023-10-23 10:54:33 -0400

    reset commit

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <main.h>
+/**
+* reset to 98 - reset value to 98
+*
+* @n: pointer integer
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This priject has 10 task about pointers, arrays and strings and one advanced task.
