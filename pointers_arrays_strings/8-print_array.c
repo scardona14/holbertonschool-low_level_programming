a4ff6be0a34672fb4e06be0c2e34ec0234986090
scardona14 <cardonasean123@gmail.com>
2023-10-24 01:59:52 -0400

    printarray commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - prints n elements of an array of integers
+* @a: array to be used
+*@n: size of array
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", a[i]);
+		if (i < n -1)
+			printf(", ");
+	}
+	printf("\n");
+}
+
