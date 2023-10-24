2a1379a50f7b8e6e79a829543a7498a905c99848
scardona14 <cardonasean123@gmail.com>
2023-10-24 02:28:41 -0400

    strcpy commit

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _strcpy - copies string pointed to by src
+* @dest: array being copied to
+* @src: string to be copied
+*
+* return: the value of pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i = 0;
+
+	while (*src++)
+		i++;
+	len = i;
+	for (i = 0, i <= len; i++)
+		dest[i] = src[i];
+	return (dest);
+}
