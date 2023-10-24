c2e81f1330da2b1b4c15a2ca88a9353d310e540a
scardona14 <cardonasean123@gmail.com>
2023-10-24 01:26:02 -0400

    putshalf commit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strlen - returns the length of a string
+* @s: variable being evaluated
+*
+* Return: length of string
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+* puts_half - prints half of string
+* @str: string being evaluated
+*/
+void puts_half(char *str)
+{
+	int i, len;
+	
+	len = strlen(str);
+	for (i = (len + 1) / 2; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
