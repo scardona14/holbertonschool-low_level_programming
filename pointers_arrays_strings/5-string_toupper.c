b1312cdc5ec77ccec33a398d03755306d0e6f320
scardona14 <cardonasean123@gmail.com>
2023-10-25 14:57:47 -0400

    stringtoupper commit

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
-* _toupper - converts lower case letters to uppercase
-* @: pointer of a string
-* Return: Pointer to the modify string
-*/
+ * *string _toupper - change all lower case to uppercase
+ * @s: pointer
+ * Return: s
+ */
 char *string_toupper(char *s)
+
 {
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
 		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] = s[i] -32;
-		}
-	}
-	return s;
+			s[i] = s[i] - 32;
+
+	return (s);
 }
