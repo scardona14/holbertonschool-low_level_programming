e5ae54a006158f8bcac557cddccdc4e6907e3156
scardona14 <cardonasean123@gmail.com>
2023-10-25 09:44:02 -0400

    strncat commit

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -13,13 +13,9 @@ char *_strncat(char *dest, char *src, int n)
 	int j = 0;
 	
 	while (dest[i])
-	{
 		i++;
-	}
 	while (j < n && src[j] != '\0')
-	{
 		dest[i++] = src[j++];
-	}
 	dest[i] = '\0';
 	return (dest);
 }
