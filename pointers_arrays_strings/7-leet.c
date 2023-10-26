40737664180cb318e928b1db754ab692bdfcf9d0
scardona14 <cardonasean123@gmail.com>
2023-10-25 23:38:15 -0400

    leet commit

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* leet - encodes a string in 1337
+* @s: string to be encoded
+*
+* return: converted string
+*/
+char *leet(char *s)
+{
+	int i, j;
+
+	char letter[] = "aAeEoOtTlL";
+	char number[] = "4433007711";
+
+	for (i = 0; s[i]; i++)
+		for (j = 0; letter[j]; j++)
+			if (letter[j] == s[i])
+				s[i] = number[j];
+	return (s);
+}
