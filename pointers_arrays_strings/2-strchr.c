a6f489659285b374eb1a060c79584045ffadf0e1
scardona14 <cardonasean123@gmail.com>
2023-10-26 13:43:35 -0400

    strchr commit

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strchar - locates a charcter in the string
+* @s: the string to search for the character
+* @c: the character to find in the string
+* Return: character value
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + 1);
+		}
+	}
+	return (0);
+}
+
