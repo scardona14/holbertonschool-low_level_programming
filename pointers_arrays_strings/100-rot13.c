75db5157d31d3d94325ae720273ffcd4d947485b
scardona14 <cardonasean123@gmail.com>
2023-10-26 02:14:50 -0400

    rot13 commit

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* rot13 - encrypts code
+* @s: string to encrypt
+* return: char value
+*/
+char *rot13(char *s)
+{
+	char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	int  i;
+	int  j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[i] == part1[j])
+			{
+				s[i] = part2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
