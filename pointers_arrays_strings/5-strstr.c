ff20f8dcca4297635afac80b76fd8fb1ec7eca64
scardona14 <cardonasean123@gmail.com>
2023-10-27 09:42:14 -0400

    strstr commit

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -3,6 +3,7 @@
 * _strstr - Locates a substring
 * @haystack: string to search in
 * @needle: substring to look for
+*
 * Return: pointer to beginning of located substring or NULL
 */
 char *_strstr(char *haystack, char *needle)
@@ -12,13 +13,12 @@ char *_strstr(char *haystack, char *needle)
 
 	if (*needle == '\0')
 		return (haystack);
-
 	for (i = 0; haystack[i]; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
 			for (j = 0; haystack[i] == needle[j]; i++, j++)
-
+				;
 			if (needle[j] == '\0')
 				return (haystack + i - j);
 		}
