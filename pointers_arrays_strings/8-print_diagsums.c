5d62d70c95d1767f8b6a85362d4ce0f3877bf32c
scardona14 <cardonasean123@gmail.com>
2023-10-27 14:38:41 -0400

    printdiagsums commit

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,11 +8,11 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int j = size - 1; 
+	int j = size - 1;
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0; i < size * size; i = i + size + 1, j = j + size -1)
+	for (i = 0; i < size * size; i = i + size + 1, j = j + size - 1)
 	{
 		sum1 = sum1 + a[i];
 		sum2 = sum2 + a[j];
