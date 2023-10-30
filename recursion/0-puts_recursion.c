d7403e20806703cce4ea33603ce6fc017a4b775b
scardona14 <cardonasean123@gmail.com>
2023-10-30 15:07:11 -0400

    putsrecursion commit

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts_recursion - Prints a string followed by a new line
+*@s: Pointer to string being evaluated
+* Return: Always 0 (success)
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
