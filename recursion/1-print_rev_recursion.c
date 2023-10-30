120b3acb7c6c21ad77e01bb31088200dd76a223a
scardona14 <cardonasean123@gmail.com>
2023-10-30 16:30:14 -0400

    revrecursion commit

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
 * _print_rev_recursion - print a string in reverse
-*@s: the string being printed
+*@s: pointer to string
 */
 void _print_rev_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
-		_print_rev_recursion(s +1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
