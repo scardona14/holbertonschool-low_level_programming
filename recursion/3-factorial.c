1b0f678b0f9efede71da15dc32b35c443e8f95fd
scardona14 <cardonasean123@gmail.com>
2023-10-31 10:34:17 -0400

    factorial commit

---
+++ recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* factorial - gives factorial of number
+* @n: number use
+* Return: returns factor or -1
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n > 0)
+		return (n * factorial(n - 1));
+	return (1);
+}
