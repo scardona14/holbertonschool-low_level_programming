81516daf8a84f726ed6d9e4a1442c6dda6ba5999
scardona14 <cardonasean123@gmail.com>
2023-11-01 13:38:17 -0400

    whatsmyname commit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point
+* @argc: counts of arguments
+* @argv: number of arguments
+* Return: Always 0
+*/
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,3 @@
+Project badge
+C - argc, argv
+This projects has 4 task and one advanced
