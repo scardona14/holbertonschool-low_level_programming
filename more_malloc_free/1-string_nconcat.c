c59d2343a4b602c7d6c05b415e2d2e046273a852
scardona14 <cardonasean123@gmail.com>
2023-11-08 14:40:40 -0400

    stringnconcat commit

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* string_nconcat - concatenates two strings
+* @s1: First string
+* @s2: Second string
+* @n: string bytes to be copied
+*
+* Return: Pointer to concatenated string, NULL if fail
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	int len1;
+	unsigned int i = 0;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	while (*(s1 + i))
+		++i;
+	len1 = i;
+	i = 0;
+	while (*(s2 + i))
+		++i;
+	if (n >= i)
+		n = i;
+	str = malloc(sizeof(char) * (len1 + n + 1));
+	if (!str)
+		return (NULL);
+	for (i = 0; *s1; ++i)
+		*(str + i) = *(s1++);
+	for (; i < len1 + n; ++i)
+		*(str + i) = *(s2++);
+	*(str + i) = '\0';
+		return (str);
+}
---
+++ more_malloc_free/RAEDME.md
@@ -0,0 +1,1 @@
+This project has 3 task and two bonus one and is about malloc
