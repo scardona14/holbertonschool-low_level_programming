e532352c39fe737d68249bc3e40e48cf173b3c1e
scardona14 <cardonasean123@gmail.com>
2023-11-13 13:55:26 -0400

    initdog commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,5 +17,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* _DOG_H_ */
