6456fd709041d8edcd15325e4c302141e76509fc
scardona14 <cardonasean123@gmail.com>
2023-11-28 13:43:48 -0400

    addnodeend commit

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+* _strlen - return the length of a string
+* @s: string being evaluated
+*
+* Return: length of the string
+*/
+int _strlen(const char *s)
+{
+	unsigned int i = 0;
+
+	while (*s++)
+		++i;
+	return (i);
+}
+/**
+* add_note_end - adds a node at the end of a list
+* @head: pointer to address of a list
+* @str: pointer to string
+*
+* Return: address of new element
+* on error, NULL
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *old = *head;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+	if (!*head)
+	{
+		*head = new;
+		return (*head);
+	}
+	while (old->next)
+		old = old->next;
+	old->next = new;
+	return (new);
+}
