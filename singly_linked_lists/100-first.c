761a05cea4d6d499a18451e2477c714cfd614655
scardona14 <cardonasean123@gmail.com>
2023-11-28 14:26:57 -0400

    100first commit

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+* first - prints a string befor main function
+*/
+__attribute__((constructor))void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my house upon my back!\n");
+}
