dbcdc5c742a6ce6f759dce0d1d42bce348608345
scardona14 <cardonasean123@gmail.com>
2023-12-07 14:45:01 -0400

    copycontentoffile commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,21 +17,21 @@ int main(int argc, char *argv[])
 	f1 = open(argv[1], O_RDONLY);
 	if (f1 < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: cant read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((n = read(f1, buf, 1024)) > 0)
 		if ((write(f2, buf, n)) != n || f2 < 0)
-		dprintf(STDERR_FILENO, "Enter: cant write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Enter: can't write to %s\n", argv[2]), exit(99);
 	if (n < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: cant read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(f1))
-		dprintf(STDERR_FILENO, "Error cant close fd %d\n", f1), exit(100);
+		dprintf(STDERR_FILENO, "Error can't close fd %d\n", f1), exit(100);
 	if (close(f2))
-		dprintf(STDERR_FILENO, "Error: cant close fd %d\n", f2), exit(100);
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", f2), exit(100);
 	return (0);
 }
