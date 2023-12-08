84b901e272ed43a030e6a4d0c7a3be1c1722d111
scardona14 <cardonasean123@gmail.com>
2023-12-08 16:06:35 -0400

    djb2 commit

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+* hash_djb2 - implements djbz alogorithm
+* @str: string to be hashed
+*
+* Return: hashed string
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 538;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
