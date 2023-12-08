055e3e121564015e3eb4695fd22f8973a430a9dc
scardona14 <cardonasean123@gmail.com>
2023-12-08 14:43:50 -0400

    hashtable commit

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - creates a hash table
+* @size - size of array
+*
+* Return: pointer to newly created hash table or NULL on failure
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	if (!size)
+		return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (!ht->array)
+	{	
+		free(ht);
+	}
+	return(NULL);
+	
+}
