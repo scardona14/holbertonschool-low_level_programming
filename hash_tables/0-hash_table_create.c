a1872a57fe590f49bbb60b618a9827bc75e6a5f2
scardona14 <cardonasean123@gmail.com>
2023-12-08 15:05:51 -0400

    hashtablecreate commit

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,11 +1,12 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_create - creates a pointer filled hash table
-* @size: size of array
-*
-* Return: pointer to newly created hash table or NULL on failure
-*/
+ * hash_table_create - creates a pointer filled hash table
+ * @size: size of array
+ *
+ * Return: pointer to newly created hash table or
+* NULL on failure
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
