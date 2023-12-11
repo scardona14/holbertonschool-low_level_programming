a10a79ce1a4c4d34801d3fd010fcbda97daff559
scardona14 <cardonasean123@gmail.com>
2023-12-10 22:27:34 -0400

    keyindex commit

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+* key_index - gives the index of a key
+* @size: size of the array of hash table
+* @key: the key to find index for
+* Return: the index of the key
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash, index = 0;
+
+	hash = hash_djb2(key);
+
+	if (size != 0)
+		index = hash % size;
+	return (index);
+}
